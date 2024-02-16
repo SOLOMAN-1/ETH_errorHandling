64f45ee7692694de33943f3495ee3e4f41720685
SOLOMAN-1 <137319284+SOLOMAN-1@users.noreply.github.com>
2024-02-16 12:05:15 +0530

    Update etherror.sol

--- etherror.sol
+++ etherror.sol
@@ -2,26 +2,34 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Error {
-    function testRequire(uint _num) public pure returns (string memory) {
-      
-        require(_num%2 == 0, "Number must be even");
-        return "It is even number";
+    function testRequirewithsum(uint _num) public pure returns (string memory) {
+        
+        require(_sumOfDigits(_num) % 2 != 0, "Sum of digits must be odd");
+        return "The sum is a odd number";
     }
 
     function testRevert(uint _num) public pure returns (string memory){
        
-        if (_num%2 != 0) {
-            revert("Number must be even");
+        if (_sumOfDigits(_num) % 2 == 0) {
+            revert("Sum must be odd");
         }
-        return "Number is even";
+        return "Sum is odd";
+    }
+    function _sumOfDigits(uint _num) internal pure returns (uint) {
+        uint sum = 0;
+        while (_num != 0) {
+            sum += _num % 10;
+            _num /= 10;
+        }
+        return sum;
     }
 
-    uint public num=8;
+   uint public  Number = 121;
 
     function testAssert() public view returns (string memory) {
        
-        assert(num%2==0);
-        return "Number is even";
+        assert(_sumOfDigits(Number) % 2 != 0);
+        return "Sum is to  be odd";
     }
 
     
